69af3cb2f89a7dcc3b299c8cbc63ef21c17574fa
Samuel Brehm <samuelbrehm@gmail.com>
2021-05-29 10:32:45 -0300

    Initial Commit

---
+++ SlidersBuilder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlidersBuilder
+//
+//  Created by Samuel Brehm on 29/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlidersBuilder/SlidersBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersBuilderApp.swift
+//  SlidersBuilder
+//
+//  Created by Samuel Brehm on 29/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SlidersBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
